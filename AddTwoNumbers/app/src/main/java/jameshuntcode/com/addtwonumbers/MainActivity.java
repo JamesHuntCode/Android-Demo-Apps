93975de47a335ac3d1e77702cd5104a14376b363
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-02-18 22:33:23 +0000

    COMPLETE APP

--- AddTwoNumbers/app/src/main/java/jameshuntcode/com/addtwonumbers/MainActivity.java
+++ AddTwoNumbers/app/src/main/java/jameshuntcode/com/addtwonumbers/MainActivity.java
@@ -2,12 +2,40 @@ package jameshuntcode.com.addtwonumbers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare widgets used
+    private EditText num1;
+    private EditText num2;
+    private Button btnAdd;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign variables
+        num1 = findViewById(R.id.etNum1);
+        num2 = findViewById(R.id.etNum2);
+        btnAdd = findViewById(R.id.btnAdd);
+        result = findViewById(R.id.tvTotal);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number1 = Integer.parseInt(num1.getText().toString());
+                int number2 = Integer.parseInt(num2.getText().toString());
+
+                int total = number1 + number2;
+
+                result.setText("Answer: " + String.valueOf(total));
+            }
+        });
     }
 }
